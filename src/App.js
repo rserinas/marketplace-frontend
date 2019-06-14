1eb5c2f0ea7184b5e0c863dcb767c3cd67f9f3dc
gfunk76 <robert@xtendly.com>
2019-06-14 15:33:26 +0800

    changes for live

--- src/App.js
+++ src/App.js
@@ -56,14 +56,14 @@ class App extends Component {
     }
     
     //************************** */
-    const is_live = false;
+    const is_live = true;
     //************************** */
 
     if (is_live) {
       sessionStorage.setItem('baseUrl', baseUrl);
       // THEA, edit also the .ph to .com
       sessionStorage.setItem('apiUrl', 'https://marketplace-api.prosperna.com');
-      sessionStorage.setItem('webBuilderApiUrl', 'https://mpwb-api.prosperna.ph/');
+      sessionStorage.setItem('webBuilderApiUrl', 'https://mpwb-api.prosperna.com/');
       // Stripe Live Key
       sessionStorage.setItem('stripeApiKey', 'pk_live_OKhCnhs83dWPNluclknbJBDG');
       // Paypal sandbox
@@ -72,13 +72,13 @@ class App extends Component {
       sessionStorage.setItem('gbURL', 'https://gw.dragonpay.ph/GenPay.aspx?merchantid=DPLPROSPERNA');
     } else {
       // This is the dev environment
-      sessionStorage.setItem('baseUrl', baseUrl);
-      sessionStorage.setItem('apiUrl', 'https://marketplace-api.prosperna.ph');
-      sessionStorage.setItem('webBuilderApiUrl', 'https://mpwb-api.prosperna.ph/');
+      // sessionStorage.setItem('baseUrl', baseUrl);
+      // sessionStorage.setItem('apiUrl', 'https://marketplace-api.prosperna.ph');
+      // sessionStorage.setItem('webBuilderApiUrl', 'https://mpwb-api.prosperna.ph/');
       // This is a local environment
-      // sessionStorage.setItem('baseUrl', 'http://localhost:3000');
-      // sessionStorage.setItem('apiUrl', 'http://localhost:8000');
-      // sessionStorage.setItem('webBuilderApiUrl', 'http://localhost:1000');
+      sessionStorage.setItem('baseUrl', 'http://localhost:3000');
+      sessionStorage.setItem('apiUrl', 'http://localhost:8000');
+      sessionStorage.setItem('webBuilderApiUrl', 'http://localhost:1000');
       // Stripe Test Key
       //sessionStorage.setItem('stripeApiKey', 'pk_test_Cd9VXBfXltI5QWfAUv6X4uRM00VGxxTg7E');// ian test keys
       sessionStorage.setItem('stripeApiKey', 'pk_test_YbL8a2pBYQTqqexvbZvZCFJJ');// prosperna stripeApiKey - pk_test_YbL8a2pBYQTqqexvbZvZCFJJ
